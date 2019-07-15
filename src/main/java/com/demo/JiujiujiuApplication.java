acaa9a5620c420d0daaae598fe5af08938fe012d
729968712@qq.com <729968712@qq.com>
2019-07-15 18:50:54 +0800

    y5

---
+++ src/main/java/com/demo/JiujiujiuApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiujiujiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiujiujiuApplication.class, args);
+    }
+
+}
